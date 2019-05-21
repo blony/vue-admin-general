4aca75daf9d4d16d18ab840d2d286b5d1dc83c4d
Bobby <h122656227@gmail.com>
2019-05-22 00:16:19 +0800

    Initialize the library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
+
+import "normalize.css/normalize.css";
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import "@/styles/index.scss"; //global css
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
