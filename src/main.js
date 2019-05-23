aa5da78abae0b2480e47e6dc3327681a4509dbe4
Bobby <h122656227@gmail.com>
2019-05-23 13:54:27 +0800

    add svg model

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@/icons"; // icon
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 "use strict";
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 const port = 8090;
 
 module.exports = {
@@ -13,6 +18,23 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    proxy: "http://localhost:9528"
+    proxy: "http://localhost:3000"
+  },
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module
+      .rule("svg")
+      .exclude.add(resolve("src/icons"))
+      .end();
+    config.module
+      .rule("icons")
+      .test(/\.svg$/)
+      .include.add(resolve("src/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]"
+      });
   }
 };
